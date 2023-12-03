c3d201cadf0b5143569a5deedbc255e719e01653
Tomer Groisman <tomergroisman@gmail.com>
2023-12-03 11:55:34 +0200

    Update inputs (change account)

--- src/main/kotlin/day01/Solution.kt
+++ src/main/kotlin/day01/Solution.kt
@@ -22,22 +22,17 @@ fun main() {
 
     fun part2(lines: List<String>): Int {
         val calibrationValues = mutableListOf<Int>()
-        val regex = Regex("(one)*(two)*(three)*(four)*(five)*(six)*(seven)*(eight)*(nine)*")
         lines.forEach {line ->
-            val parsedLine = regex.replace(line) {
-                when (it.value) {
-                    "one" -> "1"
-                    "two" -> "2"
-                    "three" -> "3"
-                    "four" -> "4"
-                    "five" -> "5"
-                    "six" -> "6"
-                    "seven" -> "7"
-                    "eight" -> "8"
-                    "nine" -> "9"
-                    else -> ""
-                }
-            }
+            val parsedLine = line
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
 
             val calibrationValue = getCalibrationValue(parsedLine)
             calibrationValues.add(calibrationValue)
