7780991c64d7dbcb83bba60c0bc1b18b17f47dce
Tomer Groisman <tomergroisman@gmail.com>
2023-12-15 11:47:01 +0200

    (Solved [Part 2]) Day 13

--- src/main/kotlin/day13/Solution.kt
+++ src/main/kotlin/day13/Solution.kt
@@ -23,29 +23,41 @@ fun main() {
         return patterns
     }
 
-    fun Pattern.findHorizontalReflectionIndex(): Int {
-        for (i in 1..<this.size) {
-            if (this[i] == this[i - 1]) {
-                var isMirror = true
-                for (j in 1..min(i - 1, this.size - 1 - i)) {
-                    if (this[i + j] != this[i - 1 - j]) {
-                        isMirror = false
-                        break
-                    }
-                }
-                if (isMirror) return i
+    fun List<Char>.diff(charList: List<Char>): Int {
+        var diff = 0
+        for (i in this.indices) {
+            if (this[i] != charList[i]) {
+                diff++
             }
+        }
+        return diff
+    }
+
+    fun Pattern.findHorizontalReflectionIndex(findSmudged: Boolean = false): Int {
+        val expectedDiffs = if (findSmudged) 1 else 0
+        for (i in 1..<this.size) {
+            var diffs = 0
+            var isMirrorCandidate = true
+            for (j in 0..min(i - 1, this.size - 1 - i)) {
+                val diff = this[i + j].diff(this[i - 1 - j])
+                diffs += diff
+                if (diff > expectedDiffs) {
+                    isMirrorCandidate = false
+                    break
+                }
+            }
+            if (isMirrorCandidate && diffs == expectedDiffs) return i
         }
         return 0
     }
 
-    fun Pattern.findVerticalReflectionIndex(): Int {
+    fun Pattern.findVerticalReflectionIndex(findSmudged: Boolean = false): Int {
         val cols = this[0].size
         val rows = this.size
         val transposedPattern = List(cols) { j ->
             List(rows) { i -> this[i][j] }
         }
-        return transposedPattern.findHorizontalReflectionIndex()
+        return transposedPattern.findHorizontalReflectionIndex(findSmudged)
     }
 
     fun part1(input: Input): Long {
@@ -53,16 +65,22 @@ fun main() {
         return patterns.sumOf {
             val verticalReflectionIndex = it.findVerticalReflectionIndex()
             val horizontalReflectionIndex = it.findHorizontalReflectionIndex()
-            (verticalReflectionIndex + 100 * horizontalReflectionIndex).toLong()
+            ReflectionLine(verticalReflectionIndex, horizontalReflectionIndex).summarize
         }
     }
 
-    fun part2(input: Input): Int {
-        return input.size
+    fun part2(input: Input): Long {
+        val patterns = input.toPatterns()
+        return patterns.sumOf {
+            val verticalReflectionIndex = it.findVerticalReflectionIndex(true)
+            val horizontalReflectionIndex = it.findHorizontalReflectionIndex(true)
+            ReflectionLine(verticalReflectionIndex, horizontalReflectionIndex).summarize
+        }
     }
 
     val testInput = readInput("$packageName/input_test")
     check(part1(testInput) == 405.toLong())
+    check(part2(testInput) == 400.toLong())
 
     val input = readInput("$packageName/input")
     part1(input).println()
@@ -70,3 +88,7 @@ fun main() {
 }
 
 typealias Pattern = List<List<Char>>
+
+data class ReflectionLine(val vertical: Int, val horizontal: Int) {
+    val summarize = (vertical + 100 * horizontal).toLong()
+}
